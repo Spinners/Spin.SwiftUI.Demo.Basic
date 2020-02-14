56eb465ff6ab578ce3d62a7e6884fe66981fbe64
Thibault Wittemberg <twittemberg@genetec.com>
2020-02-14 15:06:33 -0500

    Initial Commit

---
+++ Spin.SwiftUI.Demo.Basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spin.SwiftUI.Demo.Basic
+//
+//  Created by Thibault Wittemberg on 2020-02-14.
+//  Copyright © 2020 Spinners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
